07dca35db6c8231eaa61ddbd50e0674419e41a96
SweetSofiMC <a01636714@itesm.mx>
2020-09-15 15:31:39 -0500

    Removed non-important features (such as matrix testing), improved file naming out

--- Convolution.py
+++ Convolution.py
@@ -19,21 +19,12 @@ image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
 kernelx = numpy.array(([-1,0,1],[-2,0,2],[-1,0,1]),numpy.float32)
 kernely = numpy.array(([-1,-2,-1],[0,0,0],[1,2,1]),numpy.float32)
-kernelxy = numpy.array(([2,4,2],[4,8,4],[2,4,2]),numpy.float32)
-kernel = numpy.array(([-2,-2,0],[-2,0,2],[0,2,2]),numpy.float32)
 
 outputx = cv.filter2D(image, -1, kernelx)
 outputy = cv.filter2D(image, -1, kernely)
-outputxy = cv.filter2D(image, -1, kernelxy)
-outputxpy = cv.filter2D(image,-1,kernel)
 output = cv.add(outputx,outputy)
 
 
 if not os.path.exists('out'):
     os.makedirs('out')
-
-cv.imwrite("out/"+args["file"]+"x.png", outputx)
-cv.imwrite("out/"+args["file"]+"y.png", outputy)
-cv.imwrite("out/"+args["file"]+"xy.png", outputxy)
-cv.imwrite("out/"+args["file"]+"xpy.png", outputxpy)
-cv.imwrite("out/"+args["file"]+".png", output)
+cv.imwrite("out/"+args["file"][:-4]+".png", output)
