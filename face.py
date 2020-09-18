83f686280c340ed2ec6d83789db04b5e77a50b88
Manuel Cota <a01637477@itesm.mx>
2020-09-18 09:06:33 -0600

    Renamed variables for more descriptive names, added styling, commented

--- face.py
+++ face.py
@@ -5,7 +5,7 @@
 #Modified by Manuel Cota
 
 import cv2 as cv
-import numpy as np
+import numpy
 import dlib
 from math import hypot
 
@@ -16,7 +16,7 @@ def face_tracker():
     _, frame = cap.read()
     rens = frame.shape[1]
     cols = frame.shape[0]
-    nose_mask = np.zeros((rens,cols), np.uint8)
+    face_mask = numpy.zeros((rens,cols), numpy.uint8)
 
     #Loading Face Detector
     detector = dlib.get_frontal_face_detector()
@@ -24,7 +24,7 @@ def face_tracker():
 
     while True:
         _, frame = cap.read()
-        nose_mask.fill(0)
+        face_mask.fill(0)
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         faces = detector(frame)
@@ -32,37 +32,64 @@ def face_tracker():
         for face in faces:
             landmarks = predictor(gray_frame, face)
 
-            #Nose coordinates
-            top_nose = (landmarks.part(29).x, landmarks.part(29).y)
-            center_nose = (landmarks.part(30).x, landmarks.part(30).y)
-            left_nose = (landmarks.part(31).x, landmarks.part(31).y)
-            right_nose = (landmarks.part(35).x, landmarks.part(35).y)
+            #Face coords
+            top_face = (landmarks.part(21).x + 1000, 
+                        landmarks.part(27).y + 1000)
 
-            nose_width = int(hypot(left_nose[0] - right_nose[0],left_nose[1] - right_nose[1]) * 1.7)
+            center_face = (landmarks.part(27).x, 
+                           landmarks.part(27).y)
 
-            nose_height = int(nose_width * 1.5)
+            left_face = (landmarks.part(21).x, 
+                         landmarks.part(21).y)
 
-            #New nose position
-            top_left = (int (center_nose[0] - nose_width / 2), int(center_nose[1] - nose_height / 2))
+            right_face = (landmarks.part(22).x, 
+                          landmarks.part(22).y)
 
-            bottom_right = (int(center_nose[0] + nose_width / 2), int(center_nose[1] + nose_height / 2))
+            face_width = int(hypot(left_face[0] - 
+                             right_face[0],
+                             left_face[1] - 
+                             right_face[1]) * 1.77)
 
-            #Adding new nose
-            nose_light = cv.resize(l_image, (nose_width, nose_height))
-            nose_light_gray = cv.cvtColor(nose_light, cv.COLOR_BGR2GRAY)
-            _, nose_mask = cv.threshold(nose_light_gray, 25, 255, cv.THRESH_BINARY_INV)
-            nose_area = frame[top_left[1]: top_left[1] + nose_height,top_left[0]: top_left[0] + nose_width]
+            face_height = int(face_width * 2.2)
 
-            nose_area_no_nose = frame[top_left[1]: top_left[1] + nose_height, top_left[0]: top_left[0] + nose_width]
+            #New face position
+            top_left = (int (center_face[0] - face_width / 2), 
+                        int(center_face[1] - face_height / 2))
 
-            final_nose = cv.add(nose_area_no_nose, nose_light)
-            frame[top_left[1]: top_left[1] + nose_height,top_left[0]: top_left[0] + nose_width] = final_nose
+            bottom_right = (int(center_face[0] + face_width / 2), 
+                            int(center_face[1] + face_height / 2))
 
-            #cv.imshow("Nose area", nose_area)
-            #cv.imshow("Nose pig", nose_light)
-            #cv.imshow("final nose", final_nose)
+            #Adding new face
+            face_light = cv.resize(l_image, (face_width, face_height))
+
+            face_light_gray = cv.cvtColor(face_light, cv.COLOR_BGR2GRAY)
+            _, face_mask = cv.threshold(face_light_gray, 
+                                        25, 
+                                        255, 
+                                        cv.THRESH_BINARY_INV)
+
+            face_area = frame[top_left[1]: top_left[1] + 
+                              face_height,top_left[0]: top_left[0] + 
+                              face_width]
+
+            face_area_no_face = frame[top_left[1]: top_left[1] + 
+                                      face_height, top_left[0]: top_left[0] +
+                                      face_width]
+
+            final_face = cv.add(face_area_no_face, face_light)
+            frame[top_left[1]: top_left[1] + 
+            face_height,top_left[0]: top_left[0] + 
+            face_width] = final_face
+
+            #Render windows, for functionality demo purposes
+            #cv.imshow("Face area", face_area)
+            #cv.imshow("Face pig", face_light)
+            #cv.imshow("final face", final_face)
+
+            #Final product
             cv.imshow("Frame", frame)
-            
+        
+        #Close windows
         key = cv.waitKey(1)
         if key == 27:
             break
